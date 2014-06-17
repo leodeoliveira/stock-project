a7659f8044c40895c358df0428ba83a506179913
ocult <anjomar@gmail.com>
2014-06-16 22:06:51 -0300

    Criação do sales_order

---
+++ controller/sales_order.php
@@ -0,0 +1,26 @@
+<?php
+ob_start();
+$message = "";
+switch ($_REQUEST["ext"]) {
+	case "put":
+		try	{
+			//$conteudo["name"] = $_REQUEST["name"];
+			//$conteudo["email"] = $_REQUEST["email"];
+			//$conteudo["password"] = $_REQUEST["password"];
+			//$setup->conn->insertQuery("sale_orders", $conteudo);
+			$message = "sucesso";
+		}
+		catch (Exception $e) {
+			$message = $e->getMessage();
+		}
+	default:
+		ob_get_clean();
+		$setup->smarty->assign("message", $message);
+		//$estrutura[0] = array("ID", "Nome", "E-mail");
+		//$estrutura[1] = array("%[id_user]%", "%[name]%", "%[email]%");
+		//$tabela = $setup->conn->getTabela("SELECT id_user, name, email FROM sale_orders", $estrutura, "users");
+		//$setup->smarty->assign("tabela", $tabela);
+		$setup->pagina(true,$setup->smarty->fetch("cad_usuario.tpl"));
+		break;
+}
+?>
