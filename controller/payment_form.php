b953a0bfa7518ab4391181067a39aa4a7ac0d25b
ocult <anjomar@gmail.com>
2014-06-16 22:47:07 -0300

    Correção para inserção do pagamento (forma e condição) e cadastro de produto.

--- controller/payment_form.php
+++ controller/payment_form.php
@@ -5,9 +5,8 @@ switch ($_REQUEST["ext"]) {
 	case "put":
 		try	{
 			$conteudo["name"] = $_REQUEST["name"];
-			$conteudo["email"] = $_REQUEST["email"];
-			$conteudo["password"] = $_REQUEST["password"];
-			$setup->conn->insertQuery("users", $conteudo);
+			$conteudo["description"] = $_REQUEST["description"];
+			$setup->conn->insertQuery("payment_methods", $conteudo);
 			$message = "sucesso";
 		}
 		catch (Exception $e) {
@@ -16,6 +15,10 @@ switch ($_REQUEST["ext"]) {
 	default:
 		ob_get_clean();
 		$setup->smarty->assign("message", $message);
+		$estrutura[0] = array("C&oacute;digo", "Descri&ccedil;&atilde;o");
+		$estrutura[1] = array("%[id_payment_method]%", "%[description]%");
+		$tabela = $setup->conn->getTabela("SELECT id_payment_method, description FROM payment_methods", $estrutura, "users");
+		$setup->smarty->assign("tabela", $tabela);
 		$setup->pagina(true,$setup->smarty->fetch("payment_form.tpl"));
 		break;
 }
--- templates/tpl/payment_form.tpl
+++ templates/tpl/payment_form.tpl
@@ -1,12 +1,13 @@
 <div class="col-md-offset-2">
 <div id="form-trans">
-<form role="form" class="form-horizontal" action="adm/user.put" method="post">
+<form role="form" class="form-horizontal" action="payment_form.put" method="post">
 	<div class="form-group">
-		<label for="name" class="col-sm-2 control-label">Descrição *</label>
+		<label for="description" class="col-sm-2 control-label">Descri&ccedil;&atilde;o *</label>
 			<div class="col-sm-5">
-				<input type="name" id="name" name="name" class="form-control" placeholder="Digite a descrição..." required="required">
+				<input type="text" id="description" name="description" class="form-control" placeholder="Digite a descri&ccedil;&atilde;o..." required="required">
 			</div>
 		</div>
 		</form>
 	</div>
+	{$tabela}
 </div>
